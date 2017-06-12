dfedf59922693c86e4e9b74b739647902f8d7bc9
Ben Samuel <Benjamin.m.samuel@gmail.com>
2017-06-12 11:11:09 -0700

    Experimenting with "property[0] of undefined bug from 6/9 meeting" -- I think that the problem was a matter of it needing to be property(entity, string) and not the other way around in .lp files.

---
+++ js/6-9-deanGameProperty0.js
@@ -0,0 +1,27 @@
+fList is:  [
+    {
+        "predicate": "property",
+        "terms": [
+            {
+                "predicate": "health"
+            },
+            {
+                "predicate": "entity",
+                "terms": [
+                    {
+                        "predicate": "e_2_XX_"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "predicate": "scalar",
+        "terms": [
+            {
+                "predicate": "10"
+            }
+        ]
+    }
+]
+zTerms:  "health"
